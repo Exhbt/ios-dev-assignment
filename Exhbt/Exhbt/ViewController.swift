061866e0d00ea890ce592a0654a331231f58a5c9
Bekzod Rakhmatov <rakhmatovbekzod1996@gmail.com>
2023-03-09 06:53:38 +0500

    Initial project setup

---
+++ Exhbt/Exhbt/ViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  Exhbt
+//
+//  Created by Rakhmatov Bekzod on 09/03/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
